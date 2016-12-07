b78e531eaaddf45d5a615ccd20c20db580c17cf8
USUARIO20-PC\Usuario01 <chikifey2122@hotmail.com>
2016-12-07 12:48:52 -0500

    calculando tiempo

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,36 +1,36 @@
 ### Ejercicio 2
 from time import time
-def principal():
-   print ("BUSCADOR DE NOMBRES")
-   palabra = str(input("Nombre a buscar: "))
-   arch = str(input("Nombre del archivo donde buscar: "))
 
-   f = open(arch,'r')
-   p=0
-   for i in f.readlines():
+print ("BUSCADOR DE NOMBRES")
+palabra = str(input("Nombre a buscar: "))
+arch = str(input("Nombre del archivo donde buscar: "))
+
+f = open(arch,'r')
+p=0
+for i in f.readlines():
         palabras1=i.split(" ")
         p=p+len(palabras1)
         
-   print("\nel total de palabras es: "+str(p))
+print("\nel total de palabras es: "+str(p))
    
 
-   repetidas = 0 
-   lines = f.readlines()
-   for line in lines:
-      palabras = line.split(' ')
-      for pa in palabras:
-         if pa==palabra:
+repetidas = 0 
+lines = f.readlines()
+for line in lines:
+    palabras = line.split(' ')
+    for pa in palabras:
+        if pa==palabra:
             repetidas = repetidas+1
 
-   print ("la palabra "+str(palabra)+" se repite "+str(repetidas)+" veces")
-   return palabra
+print ("la palabra "+str(palabra)+" se repite "+str(repetidas)+" veces")
 
 def creartxt():
-   archi=open('Numero_repetidas.txt','a')
-   archi.close
-   archi=open('Numero_repetidas.txt','a')
-   archi.write('\nEl nombre se repite :'+str(repetidas)+' veces')
-   archi.close
+                archi=open('Numero_repetidas.txt','a')
+                archi.close
+
+archi=open('Numero_repetidas.txt','a')
+archi.write('\nEl nombre se repite :'+str(repetidas)+' veces')
+archi.close
             
 
 #funcion
@@ -45,6 +45,7 @@ def duracion (n):
 
 def main():
     print("i\t Tiempo de duracion de la busqueda de palabras")
-    palabra=principal()
-    print(palabra,"\t",duracion(palabra))
+    for i in range (10):
+        n=i*1000
+        print(n,"\t",duracion(n))
 main()

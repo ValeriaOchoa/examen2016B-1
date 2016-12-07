9c3eba63868a471a3dec26f4b8bb8bf3f9f49de2
cawi123 <michael_200@hotmail.es>
2016-12-07 12:12:50 -0500

    primera funcion acabada es decir la primera figura

--- ejercicio3_examen.py
+++ ejercicio3_examen.py
@@ -3,6 +3,8 @@ def rectangular ():
     altura=int(input("ingrese la altura de la pisina rectangular"))
     largo=int(input("ingrese el largo de la pisina "))
     ancho= int(input("ingrese el ancho de la piscina"))
+    volumen=altura*largo*ancho
+    print("el volumen es",volumen,"metros cubicos")
     
     
 
@@ -15,6 +17,12 @@ def main():
     print("Bienvenido eliga una opcion  ")
     print("ver a la piscina de modo  rectangular volumen")
     print("ver a la piscina de modo eliptica volumen")
-    print("Ver a la psiicina de modo circular volumen)
+    print("Ver a la psiicina de modo circular volumen")
+    choice=int(input(""))
+
+    if choice ==1:
+        rectangular()
+
+main()
   
     

9fabddb53e818d72b53944a7e5d47ff35a69f263
USUARIO18-PC\Usuario01 <www.djdanilo@hotmail.com>
2016-12-07 11:39:13 -0500

    menu con opciones

--- Menu Con Opciones.py
+++ Menu Con Opciones.py
@@ -7,44 +7,17 @@ def menu():
     print("3.- Calcular el volumen de una piscina\n")
     print("4.- Salir.....\n")
 
-def Fibo():
-    print("\tSERIE FIBONACCI")
-    a,b=0,1
-    while b<100:
-        print(b)
-        a,b=b,a+b
+def crear_archi():
+    print("\tCree un archivo de texto")
     
-def Det_Par():
-     print("\DETERMINA SI UN NUMERO ES PAR O NO")
-     numero=int(input("\tINGRESE UN NUMERO\n"))
-     if numero % 2 == 0:
-         print("EL NUMERO ES PAR")
-     else:
-         print("EL NUMERO ES IMPAR")
-         
-def Divisible():
-     print("\tDETERMINA SI UN NUMERO ES DIVISIBLE PARA 3")
-     numero=int(input("\tINGRESE UN NUMERO\n"))
-     if numero % 3 == 0:
-         print("EL  NUMERO INGRESADO ES DIVISIBLE PARA 3")
-     else:
-         print("EL  NUMERO INGRESADO NO ES DIVISIBLE PARA 3 ")
-         
-def Facto():
-    print("\tFACTORIAL")
-    f=int(1)
-    n=int(input("ingrese un numero: "))
-    while(n!=0):
-        f=f*n
-        n=n-1
-        print(f)
+    
+def leer_archi():
+     print("\Entrada de archivos")
+     
 
-def Poten():
-    print("\tPOTENCIA")
-    x=int(input("ingrese base: "))
-    p=int(input("ingrese exponente"))
-    z=(x**p)
-    print(z)
+def vol_pis():
+    print("\tCalculo volumen de una piscina")
+   
         
 def main():
     opcion=1
@@ -54,13 +27,13 @@ def main():
      opcion=int(input())
      
      if opcion == 1:
-        Fibo()
+        crear_archi()
         
      elif opcion == 2:
-        Det_Par()
+        leer_archi()
         
      elif opcion == 3:
-        Divisible()
+        vol_pis()
 
      elif opcion==4:
         print ("Has salido de la aplicación")

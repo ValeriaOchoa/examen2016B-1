c5befbf3fb7d1bb47d6192a2fb90f524f0156681
cawi123 <michael_200@hotmail.es>
2016-12-07 12:01:14 -0500

    creamos el menu para elegr el modo de la psiicina y estoy cogiendo datos en la pisicina rectangular

---
+++ ejercicio3_examen.py
@@ -0,0 +1,20 @@
+def rectangular ():
+    print ('Volume_rectangular')
+    altura=int(input("ingrese la altura de la pisina rectangular"))
+    largo=int(input("ingrese el largo de la pisina "))
+    ancho= int(input("ingrese el ancho de la piscina"))
+    
+    
+
+
+
+def main():
+#Opcion
+    choice=""
+#Menu
+    print("Bienvenido eliga una opcion  ")
+    print("ver a la piscina de modo  rectangular volumen")
+    print("ver a la piscina de modo eliptica volumen")
+    print("Ver a la psiicina de modo circular volumen)
+  
+    

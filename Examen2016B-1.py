11ae9f08ec934e99f99d2edbd2482ca650d9c465
cawi123 <michael_200@hotmail.es>
2016-12-07 14:18:30 -0500

    final

--- Examen2016B-1.py
+++ Examen2016B-1.py
@@ -85,24 +85,32 @@ def repetir():
 def main2():
 #Opcion
     choice=""
-#Menu
-    print("Bienvenido eliga una opcion  ")
-    print("(1)Ver a la piscina de modo  rectangular volumen")
-    print("(2)Ver a la piscina de modo eliptica volumen")
-    print("(3)Ver a la psiicina de modo circular volumen")
-    
-    print("ingrese la opcion")
-    choice=int(input(""))
+    while(choice!=3):
+        print("Bienvenido eliga una opcion  ")
+        print("(1)Ver a la piscina de modo  rectangular volumen")
+        print("(2)Ver a la piscina de modo eliptica volumen")
+        print("(3)Ver a la psiicina de modo circular volumen")
+        print("digite cero para salir")
+        print("ingrese la opcion")
+        choice=int(input(""))
+        if choice ==1:
+            rectangular()
+            repetir()
+        elif choice ==2:
+            eliptica()
+            repetir()
+        elif choice ==3:
+            radio()
+            repetir()
+        elif choice==0:
+            print("ADIOS")
+            exit()
+        else:
+            print("opcion incorrecta")
+            print("vuelva a ingresar")
 
-    if choice ==1:
-        rectangular()
-        repetir()
-    if choice ==2:
-        eliptica()
-        repetir()
-    if choice ==3:
-        radio()
-        repetir()
+
+
 
 
 ##Fin Ejercicio3
